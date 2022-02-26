923df41a452b32159d22c9a9cef2c2ef03a052b5
AvidHuynh <Thinhhhuynhngoc82@gmail.com>
2022-02-26 23:58:21 +0700

    hanlderPagination

--- js/home.js
+++ js/home.js
@@ -1,6 +1,6 @@
 import dayjs from 'dayjs'
 import postApi from './api/postAPI'
-import { setTextContent, truncateText } from './utils'
+import { getUlPagination, setTextContent, truncateText } from './utils'
 import relativeTime from 'dayjs/plugin/relativeTime'
 
 dayjs.extend(relativeTime)
@@ -39,35 +39,56 @@ function renderPostList(postList) {
   if (!Array.isArray(postList) || postList.length === 0) return
   const ulElement = document.getElementById('postList')
   if (!ulElement) return
+  // clear current list
+  ulElement.textContent = ''
+
   postList.forEach((post) => {
     const liElement = createPostElement(post)
     ulElement.appendChild(liElement)
   })
 }
 
-function handlePrevPagination(e) {
-  e.preventDefault()
-  console.log('prev')
-}
-
-function handleNextPagination(e) {
-  e.preventDefault()
-  console.log('next')
-}
-
 function initPagination() {
-  const ulPagination = document.getElementById('postsPagination')
+  const ulPagination = getUlPagination()
   if (!ulPagination) return
-
   const prevPagination = ulPagination.firstElementChild?.firstElementChild
   if (prevPagination) {
-    prevPagination.addEventListener('click', handlePrevPagination)
+    prevPagination.addEventListener('click', handlePrevClick)
   }
-
   const nextPagination = ulPagination.lastElementChild?.lastElementChild
   if (nextPagination) {
-    nextPagination.addEventListener('click', handleNextPagination)
+    nextPagination.addEventListener('click', handleNextClick)
   }
+}
+
+function handlePrevClick(e) {
+  e.preventDefault()
+  const ulPagination = getUlPagination()
+  if (!ulPagination) return
+  const page = Number.parseInt(ulPagination.dataset.page) || 1
+  if (page <= 1) return
+  handleFilterChange('_page', page - 1)
+}
+
+function handleNextClick(e) {
+  e.preventDefault()
+  const ulPagination = getUlPagination()
+  if (!ulPagination) return
+  const totalPages = ulPagination.dataset.totalPages
+  const page = Number.parseInt(ulPagination.dataset.page) || 1
+  if (page >= totalPages) return
+  handleFilterChange('_page', page + 1)
+}
+
+async function handleFilterChange(filterName, filterValue) {
+  // update query params
+  const url = new URL(window.location)
+  url.searchParams.set(filterName, filterValue)
+  history.pushState({}, '', url)
+
+  const { data, pagination } = await postApi.getAll(url.searchParams)
+  renderPostList(data)
+  renderPagination(pagination)
 }
 
 function initURL() {
@@ -75,8 +96,26 @@ function initURL() {
   const url = new URL(window.location)
   // update search params if needed
   if (!url.searchParams.get('_page')) url.searchParams.set('_page', 1)
-  if (!url.searchParams.get('_limit')) url.searchParams.set('_limit', 6)
+  if (!url.searchParams.get('_limit')) url.searchParams.set('_limit', 12)
   history.pushState({}, '', url)
+}
+
+function renderPagination(pagination) {
+  const ulPagination = getUlPagination()
+  if (!ulPagination || !pagination) return
+  // calc totalPages
+  const { _page, _limit, _totalRows } = pagination
+  const totalPages = Math.ceil(_totalRows / _limit)
+  // save page and totalPages to ulPagination
+  ulPagination.dataset.page = _page
+  ulPagination.dataset.totalPages = totalPages
+  // check if enable/disabled prev/next links
+  // prev
+  if (_page <= 1) ulPagination.firstElementChild?.classList.add('disabled')
+  else ulPagination.firstElementChild?.classList.remove('disabled')
+  // next
+  if (_page >= totalPages) ulPagination.lastElementChild?.classList.add('disabled')
+  else ulPagination.lastElementChild?.classList.remove('disabled')
 }
 
 ;(async () => {
@@ -86,8 +125,10 @@ function initURL() {
     initURL()
     const queryParams = new URLSearchParams(window.location.search)
     // set default query parmas if not existed
+    // fetch data from API
     const { data, pagination } = await postApi.getAll(queryParams)
     renderPostList(data)
+    renderPagination(pagination)
   } catch (error) {
     console.log('get all failed', error)
   }
